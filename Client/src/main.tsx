cce5dd992690f9d38f03d64ac86ab2c41cbce2e0
AmirAabibi79 <69202167+AmirAabibi79@users.noreply.github.com>
2024-05-12 16:43:44 +0330

    Use dayjs forLocalizationProvider instead of moment

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
 import { RecoilRoot } from "recoil";
-import "moment/locale/en-gb";
 import "./index.css";
 import App from "./App";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="en-gb">
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en-gb">
       <RecoilRoot>
         <App />
       </RecoilRoot>
